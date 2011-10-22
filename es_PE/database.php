5908c0716b15bb351b54342d8a1689cc0db01763
Johna <johna@johna-compu.(none)>
2011-10-22 01:26:50 -0500

    agregando es_Pe

---
+++ es_PE/database.php
@@ -0,0 +1,6 @@
+<?php
+	$lang = array(
+	'error' => 'Database error: %s',
+	'table_not_found' => 'La Tabla "%s" no ha sido econtrada en la base de datos. Por favor, asegúrate de estar usando la última versión de la base de datis para esta versión de Ushahidi',
+	);
+?>
---
+++ es_PE/sharing.php
@@ -0,0 +1,32 @@
+<?php
+	$lang = array(
+	'contact' => 'Contacto',
+	'date' => 'Fecha de ingreso',
+	'date_added' => 'Agregado el',
+	'last_access' => 'Último acceso',
+	'sent_info' => 'Esta información será enviada con esta solicitud',
+	'sharing_color' => array(
+		'length' => 'El campo color debe tener 6 caracteres de largo.',
+		'required' => 'El campo color es obligatorio.',
+	),
+	'sharing_email' => array(
+		'email' => 'El campo email  no parece tener una dirección de email válida?',
+		'required' => 'Su sitio de email es requerido. Por favor ve a Configuración para agregar una dirección de email.',
+	),
+	'sharing_key' => 'Llave',
+	'sharing_limits' => array(
+		'between' => 'Los límites de acceso no parecen ser válidos?',
+		'required' => 'El campo Limites de Acceso es requerido.',
+	),
+	'sharing_type' => array(
+		'between' => 'La forma de compartir parece no ser válido?',
+	),
+	'sharing_url' => array(
+		'edit' => 'No puedes editar una URL de un sitio remoto. Esta forma de compartir puede ser borrada y elaborar una nueva.',
+		'exists' => 'La URL del sitio ya existe',
+		'length' => 'La URL del sitio no es válida.',
+		'required' => 'La URL del sitio es obligatoria.',
+		'url' => 'El campo URL del sitio parece no contener una URL válida?',
+		'valid' => 'La URL del sitio no parece ser válida para ser usada por Ushahidi, o no está habilitada para compartir.',
+	));
+?>
